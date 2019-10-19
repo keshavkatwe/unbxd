4be864b3509611106fda75a953f4a0db2ad9f6d3
Keshav Katwe <keshav.ka@zeta.tech>
2019-10-19 12:02:48 +0530

    added https for the api

--- src/scripts/services/productSvc.js
+++ src/scripts/services/productSvc.js
@@ -1,7 +1,7 @@
 function getProducts({search = "*"}) {
   search = search || "*";
   return fetch(
-      'http://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?&q=' + search)
+      'https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?&q=' + search)
   .then(res => res.json());
 }
 
